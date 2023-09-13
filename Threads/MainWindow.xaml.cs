29481fe65bca791db17847cfebd41180331a02c8
martsiv <mykola.martsiv@gmail.com>
2023-09-13 11:36:27 +0300

    Completed homework Threads

--- Threads/MainWindow.xaml.cs
+++ Threads/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -36,8 +37,18 @@ namespace ThreadsL
         LongListToCommaSeparatedStringConverter converter = new LongListToCommaSeparatedStringConverter();
         Thread threadPrimeNumbers;
         Thread threadFibonacci;
+        CancellationTokenSource cts_prime = new CancellationTokenSource();
+        CancellationToken token_prime;
+        ManualResetEvent resetEvent_prime = new ManualResetEvent(true);
+        bool isGeneratingPrimes = false;
+        CancellationTokenSource cts_fibonacci = new CancellationTokenSource();
+        CancellationToken token_fibonacci;
+        ManualResetEvent resetEvent_fibonacci = new ManualResetEvent(true);
+        bool isGeneratingFibonacci = false;
+
         public MainWindow()
         {
+            token_prime = cts_prime.Token;
             InitializeComponent();
             Binding binding = new Binding("PrimeNumbers");
             binding.Converter = converter;
@@ -56,8 +67,13 @@ namespace ThreadsL
 
         private void ButtonGenerateRange_Click(object sender, RoutedEventArgs e)
         {
-            if (threadPrimeNumbers != null)
+            StartNewRange();
+        }
+        private void StartNewRange()
+        {
+            if (isGeneratingPrimes == true)
                 return;
+            isGeneratingPrimes = true;
             string? s_start = TextBoxLeftRange.Text;
             string? s_end = TextBoxRightRange.Text;
             long? start = null;
@@ -70,21 +86,23 @@ namespace ThreadsL
 
             if (start == null)
                 start = 2;
-            RangeTwoNumbers range = new RangeTwoNumbers(collection: PrimeNumbers, start: start, end: end);
+            cts_prime = new();
+            token_prime = cts_prime.Token;
+            RangeTwoNumbers range = new RangeTwoNumbers(collection: PrimeNumbers, token: token_prime, resetEvent_prime, start: start, end: end);
 
 
             threadPrimeNumbers = new Thread(GenerateRange);
             threadPrimeNumbers.Start(range);
-
         }
-
         private void GenerateRange(object obj)
         {
-            long i = 0;
-            long? start = null;
-            long? end = null;
+            CancellationToken token = ((RangeTwoNumbers)obj).token;
             try
             {
+                EventWaitHandle ev = ((RangeTwoNumbers)obj).ev;
+                long? start = null;
+                long? end = null;
+
                 if (obj is RangeTwoNumbers range)
                 {
                     if (range.Start != null)
@@ -98,8 +116,10 @@ namespace ThreadsL
                         start = tmp;
                     }
 
-                    for (i = start.Value; (end != null && i <= end) || (end == null && true); i++)
+                    for (long i = start.Value; (end != null && i <= end) || (end == null && true); i++)
                     {
+                        //wait
+                        ev.WaitOne();
                         PrimeNumbers.Add(i);
                         Application.Current.Dispatcher.Invoke(new Action(() =>
                         {
@@ -109,29 +129,20 @@ namespace ThreadsL
 
                             myTextBlock.SetBinding(TextBlock.TextProperty, binding);
                         }));
+                        token.ThrowIfCancellationRequested();
                         Thread.Sleep(1000);
                     }
                 }
             }
-            catch (ThreadAbortException e)
+            catch (OperationCanceledException e)
             {
-                Thread.ResetAbort();
-                for (; (end != null && i <= end) || (end == null && true); i++)
-                {
-                    PrimeNumbers.Add(i);
-                    Application.Current.Dispatcher.Invoke(new Action(() =>
-                    {
-                        Binding binding = new Binding("PrimeNumbers");
-                        binding.Converter = converter;
-                        binding.StringFormat = "{0}, ";
-
-                        myTextBlock.SetBinding(TextBlock.TextProperty, binding);
-                    }));
-                    Thread.Sleep(1000);
-                }
+                MessageBox.Show("Block try catch");
             }
             finally
             {
+                resetEvent_prime.Set();     // set to signaled state
+                MessageBox.Show("Block finaly");
+                isGeneratingPrimes = false;
 
             }
 
@@ -139,29 +150,30 @@ namespace ThreadsL
 
         private void StopThreadRangeButton_Click(object sender, RoutedEventArgs e)
         {
-            threadPrimeNumbers.Suspend();
+            cts_prime.Cancel();
         }
 
         private void PauseThreadRangeButton_Click(object sender, RoutedEventArgs e)
         {
-            //if (threadPrimeNumbers != null && threadPrimeNumbers.ThreadState == ThreadState.Running) 
-            threadPrimeNumbers.Abort();
+            resetEvent_prime.Reset();
         }
 
         private void ResumeThreadRangeButton_Click(object sender, RoutedEventArgs e)
         {
-            //if (threadPrimeNumbers != null && threadPrimeNumbers.ThreadState == ThreadState.Aborted) 
-            threadPrimeNumbers.Resume();
+            resetEvent_prime.Set();
         }
 
         private void ResetThreadRangeButton_Click(object sender, RoutedEventArgs e)
         {
-
+            cts_prime.Cancel();
+            StartNewRange();
         }
-        private void GenerateFibonacci()
+        private void GenerateFibonacci(object obj)
         {
+            CancellationToken token = ((CancellationToken)obj);
             try
             {
+
                 if (FibonacciNumbers.Count < 2)
                 {
                     FibonacciNumbers.Add(0);
@@ -178,6 +190,7 @@ namespace ThreadsL
                 });
                 for (int i = 2; true; i++)
                 {
+                    resetEvent_fibonacci.WaitOne();
                     long nextFibonacci = FibonacciNumbers[i - 1] + FibonacciNumbers[i - 2];
                     FibonacciNumbers.Add(nextFibonacci);
                     Application.Current.Dispatcher.Invoke(() =>
@@ -189,51 +202,56 @@ namespace ThreadsL
 
                         myTextBlockFibonacci.SetBinding(TextBlock.TextProperty, binding1);
                     });
+                    token.ThrowIfCancellationRequested();
                     Thread.Sleep(1000);
                 }
-
             }
-            catch (ThreadAbortException e)
+            catch (OperationCanceledException e)
             {
-                Thread.ResetAbort();
-                for (int i = FibonacciNumbers.Count; true; i++)
-                {
-                    long nextFibonacci = FibonacciNumbers[i - 1] + FibonacciNumbers[i - 2];
-                    FibonacciNumbers.Add(nextFibonacci);
-                }
+                MessageBox.Show("Block try catch");
             }
             finally
             {
-
+                resetEvent_fibonacci.Set();
+                MessageBox.Show("Block finaly");
+                isGeneratingFibonacci = false;
             }
-
         }
+
         private void ButtonGenerateFibonacci_Click(object sender, RoutedEventArgs e)
         {
-            if (threadFibonacci != null)
-                return;
-            threadFibonacci = new Thread(GenerateFibonacci);
-            threadFibonacci.Start();
+            StartFibonacciNumbers();
         }
+        private void StartFibonacciNumbers()
+        {
+            if (isGeneratingFibonacci == true)
+                return;
+            isGeneratingFibonacci = true;
+            cts_fibonacci = new();
+            token_fibonacci = cts_fibonacci.Token;
+            threadFibonacci = new Thread(GenerateFibonacci);
+            threadFibonacci.Start(token_fibonacci);
 
+        }
         private void StopThreadFibonacciButton_Click(object sender, RoutedEventArgs e)
         {
-            threadFibonacci.Suspend();
+            cts_fibonacci.Cancel();
         }
 
         private void PauseThreadFibonacciButton_Click(object sender, RoutedEventArgs e)
         {
-            threadFibonacci.Abort();
+            resetEvent_fibonacci.Reset();
         }
 
         private void ResumeThreadFibonacciButton_Click(object sender, RoutedEventArgs e)
         {
-            threadFibonacci.Resume();
+            resetEvent_fibonacci.Set();
         }
 
         private void ResetThreadFibonacciButton_Click(object sender, RoutedEventArgs e)
         {
-
+            cts_fibonacci.Cancel();
+            StartFibonacciNumbers();
         }
     }
     struct RangeTwoNumbers
@@ -241,11 +259,15 @@ namespace ThreadsL
         public long? Start { get; set; }
         public long? End { get; set; }
         public ObservableCollection<long> collection { get; set; }
-        public RangeTwoNumbers(ObservableCollection<long> collection, long? start = null, long? end = null)
+        public CancellationToken token { get; set; }
+        public EventWaitHandle ev { get; set; }
+        public RangeTwoNumbers(ObservableCollection<long> collection, CancellationToken token, EventWaitHandle ev, long? start = null, long? end = null)
         {
             this.collection = collection ?? new ObservableCollection<long>();
             Start = start;
             End = end;
+            this.token = token;
+            this.ev = ev;
         }
     }
     [AddINotifyPropertyChangedInterface]
